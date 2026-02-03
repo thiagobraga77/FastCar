4a86a61ac326903f489a284f1fac68fddcb55992
Thiago Braga <thiagobraga2a@gmail.com>
2026-02-03 14:11:10 -0300

    feat: adiciona models, controllers e migrations

---
+++ app/Models/Locacao.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Locacao extends Model
+{
+    /** @use HasFactory<\Database\Factories\LocacaoFactory> */
+    use HasFactory;
+}
