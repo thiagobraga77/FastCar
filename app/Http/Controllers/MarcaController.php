f7d6193cd1d2b298160f046adeaba70015d52c02
Thiago Braga <thiagobraga2a@gmail.com>
2026-02-17 09:09:06 -0300

    feat: adiciona registros via POST

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -28,7 +28,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $marca = Marca::create($request->all());
+        return $marca;
     }
 
     /**
