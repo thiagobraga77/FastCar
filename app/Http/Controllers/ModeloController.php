59c6d8c974d8235faabe3db6c4f7be87cc0da0d6
Thiago Braga <thiagobraga2a@gmail.com>
2026-03-31 15:45:24 -0300

    feat: adiciona condições nas pesquisas

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -29,6 +29,12 @@ class ModeloController extends Controller
         } else {
             $modelos = $this->modelo->with('marca');
         }
+
+        if($request->has('filtro')) {
+            $condicoes = explode(':', $request->filtro);
+            $modelos = $modelos->where($condicoes[0], $condicoes[1], $condicoes[2]);
+            
+        }
         
         if($request->has('atributos')){
             $atributos = $request->atributos;
