93a6186b621761a7e56c485231d684c9cf89392a
Thiago Braga <thiagobraga2a@gmail.com>
2026-04-01 17:57:30 -0300

    feat: adiciona um Repository para Marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\MarcaRepository;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
 use Illuminate\Http\Request;
@@ -25,35 +26,27 @@ class MarcaController extends Controller
      */
     public function index(Request $request)
     {
-         $marcas = array();
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if($request->has('atributos_modelos')) {
             $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marca->with('modelos:id,'.$atributos_modelos);
+            $atributos_modelos = 'modelos:id,'.$atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
         if($request->has('filtro')) {
-            $filtros = explode(';', $request->filtro);
-            foreach($filtros as $key => $condicao) {
-
-                $c = explode(':', $condicao);
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-
-            }
+           $marcaRepository->filtro($request->filtro);
         }
 
         if($request->has('atributos')) {
-            $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
-        } else {
-            $marcas = $marcas->get();
-        }
-        // Listar todos os regitros;
-        $marcas = $this->marca->with()->get(); // usando a instância do objeto
+            $marcaRepository->selectAtributos($request->atributos);
+        } 
+
+
         // $marcas =  Marca::all(); // não fazemos a instância do objeto
-        return response()->json($marcas,200);
+        return response()->json($marcaRepository->getResultado(),200);
     }
 
     /**
@@ -236,4 +229,6 @@ Rules -> São regras de validação de dados. Elas definem o que é permitido
 ou não quando alguém envia dados para o sistema.
 As rules garantem que os dados estejam corretos antes de serem salvos no banco.
 
+Repository Design Pattern -> solução para evitar a repetição de código na manipulação de dados em aplicações. Evitar 
+a duplicação de código e torna manutenção mais fácil.
 */ 
