acac5aaa242d1c23b4bad74903af9df32ca47cc4
Thiago Braga <thiagobraga2a@gmail.com>
2026-04-10 10:29:35 -0300

    feat: adiciona o método de login(autenticação) e o JWT(autorização)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,22 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+        $credenciais = $request->all(['email', 'password']);
+        //autenticação(email e senha)
+
+        $token = auth('api')->attempt($credenciais);
+
+        if($token){ //caso o usuário seja autenticado com sucesso
+            return response()->json(['token' => $token]);
+        } else {
+            return response()->json(['erro' => 'Usuário ou senha inválido!'], 403);
+
+            //401 = Unauthorized -> não autorizado
+            //403 = forbidden -> proibido(login invertido)
+        }
+        
+        //retornar um token(JWT)
         return 'login';
     }
     public function logout(){
